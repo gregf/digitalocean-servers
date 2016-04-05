bd60834937bef4477675b558f4793acd7b2b6ddc
Greg Fitzgerald <greg@gregf.org>
2016-04-05 10:49:37 -0400

    Split tf template into sperate files

---
+++ web.tf
@@ -0,0 +1,39 @@
+# Set the variable value in *.tfvars file
+variable "do_token" {}
+variable "pub_key" {}
+variable "pvt_key" {}
+variable "ssh_fingerprint" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
+
+connection {
+  user = "root"
+  type = "ssh"
+  key_file = "${var.pvt_key}"
+  timeout = "2m"
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "apt-get update",
+    "apt-get upgrade -y",
+    "apt-get dist-upgrade -y"
+    "apt-get install -y sudo"
+    "reboot"
+  ]
+}
+
+resource "digitalocean_droplet" "web" {
+  image = "debian-8-3-x64"
+  name = "www.gregf.org"
+  region = "nyc3"
+  size = "1gb"
+  ipv6 = true
+  private_networking = true
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
+}
